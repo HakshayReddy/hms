2ee4d5ff85150dbfab2a6ea755316618250ff567
Hakshayreddy <hakshay2005@gmail.com>
2024-09-24 23:58:21 +0530

    V0.6

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const password = document.getElementById("password").value;
 
     if (role && username && password) {
-      if (verifyCreds(role, username, password)) {
+      if (verifyCreds(role, username, password) == true) {
         // Redirect based on selected role
         switch (role) {
           case "student":
