0f3865b6eebaf3d0f7a49f2f9254fb2b7eb5ec98
Hakshayreddy <hakshay2005@gmail.com>
2024-09-24 23:37:36 +0530

    V0.5

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     console.log(pass);
     console.log(roleDB);
+    console.log(role);
     return roleDB == role && pass==password;
 }
   form.addEventListener("submit", async function (event) {
