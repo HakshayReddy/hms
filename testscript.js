76d803e5ea60c68224f1febdb9b7fe67d6630521
Hakshayreddy <hakshay2005@gmail.com>
2024-09-24 22:26:21 +0530

    V0.4

--- testscript.js
+++ testscript.js
@@ -6,26 +6,24 @@ btn.addEventListener("click", function () {
 });
 
 // Replace with your actual API Gateway URL
-const apiUrl = "https://0ey5491w6i.execute-api.eu-north-1.amazonaws.com/verifyCreds";
-function getStudentEmail(studentID) {
+async function getStudentEmail(studentID) {
     // Make an API call to the backend to get the student's email
-    fetch(apiUrl , {
-        method: 'GET',
-        mode: 'no-cors',  // Add no-cors mode to bypass CORS policy
+const apiUrl = "https://0ey5491w6i.execute-api.eu-north-1.amazonaws.com/stage1/verifyCreds";
+
+    fetch(apiUrl, {
+        method: 'GET', 
+        // mode: 'no-cors',
         headers: {
-            'Content-Type': 'application/json'  
-        }})
-        .then(response => response.json())
-        .then(data => {
-            if (data.email) {
-                console.log('Student Email:', data.email);
-                document.getElementById('emailOutput').innerText = 'Email: ' + data.email;
-            } else {
-                console.log('Error:', data.message);
-                document.getElementById('emailOutput').innerText = 'Error: ' + data.message;
-            }
-        })
-        .catch(error => {
-            document.getElementById('emailOutput').innerText = 'Error: ' + error.message;
-        });
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin' : "*",
+          'Access-Control-Allow-Credentials': true,
+        }
+      })  
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 }
